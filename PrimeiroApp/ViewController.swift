c7ba036032452bb00b2696ca29a8ae86226c9ad8
Jauri da Cruz Junior <jauri.dev@gmail.com>
2023-01-19 16:41:48 -0300

    My first ios app

---
+++ PrimeiroApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimeiroApp
+//
+//  Created by Jauri da Cruz Junior on 16/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello World!")
+    }
+
+
+}
+
